3bfbf18f82f44e0dbfb281560f20cb61ebe7a855
jingyuanhe <389026847@qq.com>
2020-01-28 14:55:36 +0800

    no message

--- pages/a.js
+++ pages/a.js
@@ -1,16 +1,23 @@
 import {Button} from 'antd'
 import { withRouter } from 'next/router'
 import React from 'react'
-
+import dynamic from 'next/dynamic'//异步加载
+const Com=dynamic(import('../components/comp.jsx'))
 const a=class extends React.Component {
   static async getInitialProps({ req,query }) {
+    const moment =await import('moment')//异步加载
     const id=query.id;
-    return {id }
+    const time=moment.default(Date.now()-60*1000).fromNow();
+    return {id,time }
   }
 
   render() {
     return (
+      <>
+      <Com></Com>
     <Button>{this.props.id}</Button>
+    <span>{this.props.time}</span>
+    </>
     )
   }
 }
