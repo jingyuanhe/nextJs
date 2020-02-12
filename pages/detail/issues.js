f534331805bbc21ef20d31c0cd145848ade72781
jingyuanhe <389026847@qq.com>
2020-02-12 16:10:26 +0800

    issues页面布局

--- pages/detail/issues.js
+++ pages/detail/issues.js
@@ -1,10 +1,110 @@
 import withRepoBasic from '../../components/with-repo-basic.jsx'
-function Issues({text}){
-return <span>{text}</span>
+const api = require('../../lib/api')
+import { Avatar,Button } from 'antd'
+import {useState,EffectCallback, useCallback} from 'react'
+import dynamic from 'next/dynamic'
+import {getLastUpdataed} from '../../lib/utils'
+const MdRenderer=dynamic(()=>import('../../components/MarkDownRender'))
+function IssueDetail({issue}){
+    return <div className="root">
+        <MdRenderer content={issue.body}></MdRenderer>
+        <div className="actions">
+            <Button href={issue.html_url} target="_blank">打开Issue讨论页面</Button>
+        </div>
+        <style jsx>{
+            `
+             .root{
+                 background:#fefefe;
+                 padding:20px;
+             }
+             .actions{
+                 text-align:right;
+             }
+            `    
+        }</style>
+    </div>
 }
-Issues.getInitialProps=async ()=>{
+function IssueItem({ issue }) {
+    const [showDetail,setShowDetail]=useState(false);
+    const toggleShowDetail=useCallback(()=>{setShowDetail(detail=>!detail)},[])
+    return (
+        <div>
+            <div className="issue">
+                <Button type="primary" size="small" style={{position:'absolute',right:20,top:20}} onClick={toggleShowDetail}>
+                    {showDetail?'隐藏':'显示'}
+                </Button>
+                <div className="avatar">
+                    <Avatar src={issue.user.avatar_url} shape="square" size={50}></Avatar>
+                </div>
+                <div className="main-info">
+                    <h6><span>{issue.title}</span></h6>
+                    <p className="sub-info">
+                        <span>Updated at {getLastUpdataed(issue.updated_at)}</span>
+                    </p>
+                </div>
+                <style jsx>
+                    {
+                        `
+                        .issue{
+                            display:flex;
+                            position:relative;
+                            padding:10px;
+                        }
+                        .issue:hover{
+                            background:#fafafa;
+                        }
+                        .issue + .issue{
+                            border-top:1px solid #eee;
+                        }
+                        .main-info > h6{
+                            max-width:600px;
+                            padding-right:40px;
+                            font-size:16px;
+                        }
+                        .avatar{
+                            margin-right:20px;
+                        }
+                        .sub-info{
+                            margin-bottom:0
+                        }
+                        .sub-info > span + span{
+                            display:inline-block;
+                            margin-left:20px;
+                            font-size:12px;
+                        }
+                        `
+                    }
+                </style>
+            </div>
+            <div>
+                {showDetail?<IssueDetail issue={issue}></IssueDetail>:null}
+            </div>
+        </div>
+    )
+}
+function Issues({ issues }) {
+    return <div className="root">
+        <div className="issues">
+            {issues.map(issue=><IssueItem issue={issue} key={issue.id}></IssueItem>)}
+        </div>
+        <style jsx>
+            {`
+                .issues{
+                    border:1px solid #eee;
+                    border-radius:5px;
+                    margin-bottom:20px;
+                    margin-top:20px;
+                }
+            `}
+        </style>
+    </div>
+}
+Issues.getInitialProps = async ({ ctx }) => {
+    const { owner, name } = ctx.query;
+    const issuresResp = await api.request({ url: `/repos/${owner}/${name}/issues` }, ctx.req, ctx.res);
+
     return {
-        text:3222
+        issues: issuresResp.data
     }
 }
-export default withRepoBasic(Issues,'issues');
+export default withRepoBasic(Issues, 'issues');
